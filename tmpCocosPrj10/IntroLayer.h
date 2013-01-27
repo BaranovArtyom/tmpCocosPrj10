85d24ebbeb7bd2815030d43c244d4026e74b41e1
Artem Baranov <artem.brnv@gmail.com>
2013-01-27 16:33:11 +0400

    Initial Commit

---
+++ tmpCocosPrj10/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  tmpCocosPrj10
+//
+//  Created by Apple on 27.01.13.
+//  Copyright Smarty 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
